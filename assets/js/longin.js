73207a76e3439f62f87ec1f998bfaf1d608dfdd4
liyanchen <329257132@qq.com>
2020-02-27 21:59:11 +0800

    '2200'

--- assets/js/longin.js
+++ assets/js/longin.js
@@ -3,29 +3,29 @@ $(function() {
   var layer = layui.layer;
   //点击去注册,跳转
   $("#link-reg").on("click", function() {
-    $(".reg-box").show();
-    $(".login-box").hide();
+    $(".reg-box").show(); //显示
+    $(".login-box").hide(); //隐藏
   });
   $("#link-login").on("click", function() {
-    $(".login-box").show();
-    $(".reg-box").hide();
+    $(".login-box").show(); //显示
+    $(".reg-box").hide(); //隐藏
   });
   //表单验证
   form.verify({
-    //这是对于注册用户名的验证
-    uName: function(value, item) {
-      //value：表单的值、item：表单的DOM对象
+    // //这是对于注册用户名的验证
+    // uName: function(value, item) {
+    //   //value：表单的值、item：表单的DOM对象
 
-      if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)) {
-        return "用户名不能有特殊字符";
-      }
-      if (/(^\_)|(\__)|(\_+$)/.test(value)) {
-        return "用户名首尾不能出现下划线'_'";
-      }
-      if (/^\d+\d+\d$/.test(value)) {
-        return "用户名不能全为数字";
-      }
-    },
+    //   if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)) {
+    //     return "用户名不能有特殊字符";
+    //   }
+    //   if (/(^\_)|(\__)|(\_+$)/.test(value)) {
+    //     return "用户名首尾不能出现下划线'_'";
+    //   }
+    //   if (/^\d+\d+\d$/.test(value)) {
+    //     return "用户名不能全为数字";
+    //   }
+    // },
     pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
     samePwd: function(value) {
       var respwd = $(".reg-box [name=password]").val();
