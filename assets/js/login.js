95ca5532059bcc5e10a12b2c267e31224e3d641f
liyanchen <329257132@qq.com>
2020-02-28 10:05:36 +0800

    完成了登录和注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,16 +1,16 @@
-$(function() {
+$(function () {
   // 只要导入了 layui.all.js 脚本，就可以使用 layui.form
   var form = layui.form;
   var layer = layui.layer;
 
   // 点击了注册的链接
-  $("#link-reg").on("click", function() {
+  $("#link-reg").on("click", function () {
     $(".login-box").hide(); // 隐藏
     $(".reg-box").show(); // 展示
   });
 
   // 点击了登录的链接
-  $("#link-login").on("click", function() {
+  $("#link-login").on("click", function () {
     $(".login-box").show(); // 展示
     $(".reg-box").hide(); // 隐藏
   });
@@ -19,7 +19,7 @@ $(function() {
   form.verify({
     // 键：值
     pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
-    samePwd: function(value) {
+    samePwd: function (value) {
       // 1. 通过形参，获取到确认密码框中的值
       // 2. 通过 jQuery 获取到密码框中的值
       var pwd = $(".reg-box [name=password]").val();
@@ -32,7 +32,7 @@ $(function() {
   });
 
   // 监听注册表单的提交事件
-  $("#form-reg").on("submit", function(e) {
+  $("#form-reg").on("submit", function (e) {
     // 1. 阻止表单的默认提交行为
     e.preventDefault();
     // 2. 发起Ajax请求
@@ -45,7 +45,7 @@ $(function() {
       // 指定请求的数据
       data: $(this).serialize(),
       // 指定成功的回调函数
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           // 注册失败
           return layer.msg(res.message);
@@ -59,7 +59,7 @@ $(function() {
   });
 
   // 监听登录表单的提交事件
-  $("#form-login").on("submit", function(e) {
+  $("#form-login").on("submit", function (e) {
     // 1. 阻止默认提交行为
     e.preventDefault();
     // 2. 手动发起 ajax 请求
@@ -67,7 +67,7 @@ $(function() {
       type: "POST",
       url: "http://www.liulongbin.top:3007/api/login",
       data: $(this).serialize(),
-      success: function(res) {
+      success: function (res) {
         // 先使用 if 判断失败的请求，然后 return 出去
         // 如果没有被 return 出去，那就是成功了
         if (res.status !== 0) {
